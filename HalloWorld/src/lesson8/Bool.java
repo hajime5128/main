5cc491873b200f4f99cdfadf963660c044c42a27
hajime <hajime@localhost>
2023-04-01 23:38:51 +0900

    課題1.８　問１．４比較演算子訂正後　再提出

--- HalloWorld/src/lesson8/Bool.java
+++ HalloWorld/src/lesson8/Bool.java
@@ -8,7 +8,7 @@ public class Bool {
 		int a = 20;
 		int b = 5;
 		boolean result_1;
-		result_1 = a > b;
+		result_1 = a >= b;
 		System.out.println(result_1);
 //		問２）
 //		以下の変数を使ってboolean型変数 result_2 に「変数 c の値と変数 d の値は等しくない」という条件式を代入して下さい。
@@ -32,7 +32,7 @@ public class Bool {
 //		boolean型変数 result_4 をコンソールに出力して下さい。
 		int g = 7;
 		boolean result_4;
-		result_4 = g < 5 || g <10;
+		result_4 = g <= 5 || g >=10;
 		System.out.println(result_4);
 
 	}
