7807d899762970513ca50478e1f58da98268ed43
hajime <hajime@localhost>
2023-04-20 22:11:22 +0900

    課題19

---
+++ HalloWorld/src/lesson19/Dog.java
@@ -0,0 +1,16 @@
+package lesson19;
+
+public class Dog {
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	String animal;
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int head;
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	public Dog() {
+		this.animal = "犬";
+	}
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	public Dog(int i) {
+		this.head = i;
+	}
+}
