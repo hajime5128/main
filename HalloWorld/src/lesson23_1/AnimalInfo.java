f3a65068addc6c1a36e6e0fe633c838d8f7c779e
hajime <hajime@yoshidarounombp>
2023-04-24 22:37:25 +0900

    課題23

--- HalloWorld/src/lesson23_1/AnimalInfo.java
+++ HalloWorld/src/lesson23_1/AnimalInfo.java
@@ -11,43 +11,46 @@
 package lesson23_1;
 
 public class AnimalInfo{
+	// 変数宣言
 	private String animal = "ライオン";
 	private double lenght = 2.1;
 	private int speed = 80;
-	
+
+	// getAnimalメゾット getter setter
 	public String getAnimal() {
 		return animal;
 	}
 
 	public void setAnimal(String animal) {
+		// nullチェック
 		if(animal == null) {
 			throw new IllegalArgumentException("名前を入力してください。");
 		}
+		// thisを使ってフィールド変数代入
 		this.animal = animal;
 		System.out.println("動物名：" + animal);
 	}
 
+	// getLenghtメゾット getter setter
 	public double getLenght() {
 		return lenght;
 	}
 
 	public void setLenght(double lenght) {
+		// thisを使ってフィールド変数代入
 		this.lenght = lenght;
 		System.out.println("体長：" + lenght + "m");
 	}
 
+	// getSpeedメゾット getter setter
 	public int getSpeed() {
 		return speed;
 	}
 
 	public void setSpeed(int speed) {
+		// thisを使ってフィールド変数代入
 		this.speed = speed;
 		System.out.println("速度：" + speed + "km/h");
 	}
 
-//	public void print() {
-//		System.out.println("動物名：" + animal);
-//		System.out.println("体長：" + lenght + "m");
-//		System.out.println("速度：" + speed + "km/h");
-//	}
 }
--- HalloWorld/src/lesson23_main/Lesson23_main.java
+++ HalloWorld/src/lesson23_main/Lesson23_main.java
@@ -1,12 +1,12 @@
 /*
- 	
+
  	下記がコンソールに出力されるように作成してください
  	※thisとsetterとgetterとフィールドを使ってください
- 	
+
   	動物名：ライオン
 	体長：2.1m
 	速度：80km/h
- 	
+
  */
 package lesson23_main;
 
@@ -14,11 +14,11 @@ import lesson23_1.AnimalInfo;
 public class Lesson23_main{
 	public static void main (String[] args) {
 		AnimalInfo animal = new AnimalInfo();
-		animal.getAnimal();
-		animal.setAnimal(animal.getAnimal());
-		animal.getLenght();
-		animal.setLenght(animal.getLenght());
-		animal.getSpeed();
-		animal.setSpeed(animal.getSpeed());
+		animal.getAnimal();						//　戻り値
+		animal.setAnimal(animal.getAnimal());	// getter変数代入
+		animal.getLenght();						//　戻り値
+		animal.setLenght(animal.getLenght());	// getter変数代入
+		animal.getSpeed();						//　戻り値
+		animal.setSpeed(animal.getSpeed());		// getter変数代入
 	}
 }
